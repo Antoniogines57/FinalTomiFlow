d8d94a33dfcb404139709abf3062e67fa33bfd47
Rafael García <themrrafus@gmail.com>
2017-11-24 23:51:36 +0100

    Añadido Estadoy y Juego de Dimitriy

---
+++ src/estados/Estados.java
@@ -0,0 +1,38 @@
+package estados;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.*;
+
+public class Estados extends StateBasedGame {
+
+	private AppGameContainer contenedor;
+	public Estados()throws SlickException {
+		super("TomiFlow");
+		contenedor= new AppGameContainer(this);
+		contenedor.setDisplayMode(640,640,false);
+		contenedor.setShowFPS(false);
+		contenedor.start();
+	}
+/**
+ * Inicializa la lista de los estados del juego
+ */
+	@Override
+	public void initStatesList(GameContainer container) throws SlickException {
+		// TODO Auto-generated method stub
+		this.addState(new Juego());
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			Estados juego = new Estados();
+		} catch(SlickException slick) {
+			slick.printStackTrace();
+			System.exit(1);
+		}
+	}
+
+}
---
+++ src/estados/Juego.java
@@ -0,0 +1,96 @@
+package estados;
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Circle;
+import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class Juego extends BasicGameState {
+
+	public Juego() {
+		// TODO Auto-generated constructor stub
+	}
+    private Rectangle rectangulo = null;
+    private Circle circulo = null;
+	@Override
+	public void init(GameContainer container, StateBasedGame game) throws SlickException {
+		//Inicializa
+        this.rectangulo = new Rectangle(10, 10, 40, 40);
+        this.circulo = new Circle(100, 100, 7);
+
+	}
+
+	@Override
+	public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
+		//Dibuja
+    	g.setColor(Color.darkGray);		// Para el rectangulo
+    	g.fill(this.rectangulo);
+    	g.setColor(Color.green);		// Para el circulo
+    	g.fill(circulo);
+    	g.draw(circulo);
+		
+		
+		
+	}
+
+	@Override
+	public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
+		// Actualiza
+    	
+		int i=1;
+		Input input = container.getInput();
+    	
+    	if (input.isKeyDown(Input.KEY_DOWN)) {			// no ponemos "else" porque sino, solo se cumplira una condicion a la vez, 
+    		rectangulo.setY(rectangulo.getY()+i);				// y nos interesa que podamos mover la bola en varias direcciones a la vez
+    	}
+    	if  (input.isKeyDown(Input.KEY_UP)){
+         rectangulo.setY(rectangulo.getY()-i); 					//Para arriba negativo y para abajo positivo
+    	}
+    	if (input.isKeyDown(Input.KEY_RIGHT)) {
+    		rectangulo.setX(rectangulo.getX()+i);
+    	}
+    	if (input.isKeyDown(Input.KEY_LEFT)) {
+    		rectangulo.setX(rectangulo.getX()-i);
+    	}
+    	if (input.isKeyPressed(Input.KEY_R)) { 			//Si se pierde el rectangulo con la r vuelve a su hogar
+    		rectangulo.setX(0);
+    		rectangulo.setY(0);}
+    	
+    	// Comprobamos colisiones
+    	
+    	if (rectangulo.intersects(circulo)) {			// Si el rectangulo toca al circulo
+    		randomPos(circulo);					// Llamamos al metodo que movera al circulo a una posicion aleatoria
+    	}
+  
+   
+	}
+
+	@Override
+	public int getID() {
+		/*
+		 * Slick relaciona cada estado con un entero, no puede estar repetido 
+		 * 
+		 * Cada estado debe de tener asociado un entero, da igual cómo se haga
+		 * 
+		 */
+		return 0;
+	}
+    public static void randomPos(Circle circulo) {			// Una funcion que asigna una posicion aleatorio al circulo dado en el argumento
+    	circulo.setX((float) (Math.random()*480));
+    	circulo.setY((float) (Math.random()*480));
+    }
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
