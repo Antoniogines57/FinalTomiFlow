91aec80764f86f84743ffce00465a0ad0c85e827
ASPIRE-TC\Dimitri <dvmdvm97@gmail.com>
2017-11-25 17:46:57 +0100

    Updated
    Added:
    -Mouse Position Strings
    -Adjusted Image's Position

--- src/testing/Menu.java
+++ src/testing/Menu.java
@@ -1,9 +1,13 @@
 package testing;
 
+import org.lwjgl.input.Mouse;
 import org.newdawn.slick.*;
 import org.newdawn.slick.state.*;
 
 public class Menu extends BasicGameState{
+	
+	public String mousePosX = "";
+	public String mousePosY = "";
 	public Menu(int state){
 	}
 	
@@ -11,12 +15,18 @@ public class Menu extends BasicGameState{
 	}
 	
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException{
-		g.drawString("Comenzar juego!", 240, 100);
+		g.drawString("Comenzar juego!", 240, 140);
 		Image startbutton = new Image("res/startgame.jpg");
-		g.drawImage(startbutton, 247, 150);
+		g.drawImage(startbutton, 247, 190);
+		g.drawString(mousePosX, 30, 40);
+		g.drawString(mousePosY, 30, 60);
 	}
 	
 	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException{
+		int xpos = Mouse.getX();
+		int ypos = Mouse.getY();
+		mousePosX = "Mouse Position X: " + xpos;
+		mousePosY = "Mouse Position Y: " + ypos;
 	}
 	
 	public int getID(){
