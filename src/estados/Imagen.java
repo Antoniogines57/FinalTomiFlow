23a5dc62dce081b471be642699eabb55ab227f72
ASPIRE-TC\Dimitri <dvmdvm97@gmail.com>
2017-11-26 18:47:20 +0100

    Arreglado
    Ahora el boton de salir funciona

--- src/estados/Imagen.java
+++ src/estados/Imagen.java
@@ -37,20 +37,16 @@ public class Imagen extends BasicGameState {
 		int posX=mouse.getMouseX();
 		int posY=mouse.getMouseY();
 		if ((posX<551)&&(posX>80)&&((posY<208)&&(posY>90))) { //Comprobamos que el mouse está en el área del botón
-			
 			if(mouse.isMouseButtonDown(0)) //Comprobamos que está presionado el botón
 			{
 				game.enterState(1);  //Entramos al modo de juego 1
-		}
-			 if ((posX<405)&&(posX>180)&&((posY<524)&&(posY>300))) { //Comprobamos que el mouse está en el área del botón
+			}}
+		if ((posX<405)&&(posX>180)&&((posY<524)&&(posY>300))) { //Comprobamos que el mouse está en el área del botón
 				
-				if(mouse.isMouseButtonDown(0)) //Comprobamos que está presionado el botón
-				{
-					System.exit(0);
-			}
-			}
+			if(mouse.isMouseButtonDown(0)) {			//Comprobamos que está presionado el botón
+				System.exit(0);
+			}}
 			
-		}
 		
 System.out.println(posY);
 
