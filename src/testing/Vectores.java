a908242e97c7cdca353ac38f47acc9aef6dacdb7
ASPIRE-TC\Dimitri <dvmdvm97@gmail.com>
2017-11-26 21:18:02 +0100

    Actualizado pero necesita arreglo
    En los comentario del codigo se explica lo que hay que arreglar

--- src/testing/Vectores.java
+++ src/testing/Vectores.java
@@ -8,30 +8,43 @@ import org.newdawn.slick.geom.*;
 
 public class Vectores extends BasicGame {
 	
-	Vector2f vector;
-	Circle circulo;
-	Circle circulo1;
+	private Vector2f gravity = new Vector2f(0,2);
+	public class Objeto extends Circle{
+		public Vector2f position;			// Vectro posicion del objeto
+		public Objeto(float x, float y, float rad) {
+			super(x, y, rad);
+			
+		}
+		public Vector2f velocity = new Vector2f(1,-1);		// Vector velocidad, modificara el vector posicion
+	}
+	Objeto objeto = new Objeto(100,100,15);
+	Line suelo = new Line(0,500,1024,500);				// El suelo, al tocarlo, el objeto se para
+	int timepassed = 0;			// la variable t, para modificar la posicion !!! es necesario modificarlo para el correcto funcionamiento
+	
 	
 	public Vectores(String gamename) {
 		super(gamename);
 	}
 
 	public void init(GameContainer gc) throws SlickException{
-		vector = new Vector2f(2,4);
-		circulo = new Circle(50, 100, 10);
-		circulo1 = new Circle(170, 230, 8);
+		objeto.velocity = objeto.velocity.add(gravity);
 	}
 	
 	public void render(GameContainer gc, Graphics g) throws SlickException{
-		g.draw(circulo);
-		g.draw(circulo1);
+		g.draw(objeto);
+		g.draw(suelo);
 	}
 	
 	public void update(GameContainer gc, int delta) throws SlickException{
-		Input input = gc.getInput();
-		if (input.isKeyDown(Input.KEY_SPACE)) {
-			vector.projectOntoUnit(vector, vector);
+		timepassed += delta;
+		objeto.position = new Vector2f(objeto.getX(), objeto.getY());				// Vector posicion
+		if (!objeto.intersects(suelo)){												// Si el objeto no toca el suelo, seguimos moviendolo
+			objeto.position = objeto.position.add(objeto.velocity.scale(1));	// La posicion del objeto cambia, modificando su vector de posicion sumandole el vector velocidad
+																						// velocity.scale() modifica el vector velocidad, habria que averiguar una forma para que la bola
+																						// no se mueva tan rapido
 		}
+		objeto.setX(objeto.position.x);		// Modificamos la posicion del objeto basandose en su vector posicion
+		objeto.setY(objeto.position.y);
 	}
 	
 	public static void main(String[] args) {
